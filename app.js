5268910f07cfbf67eb0b0b31997be06e00460eff
fatemehrng <fatemehrng@gmail.com>
2019-08-07 16:51:05 +0430

    new text file added

--- app.js
+++ app.js
@@ -59,8 +59,8 @@ app.use(session({
   }
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -77,6 +77,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+  res.status(err.status || 500);
   res.status(err.status || 500);
   res.render('error');
 });
